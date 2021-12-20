721b04189a83a927863206d9bc2fc0068cb35a9a
Orazio <orazioventrella@hotmail.it>
2021-12-20 17:30:05 +0100

    es 3

---
+++ es-3/index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>es-3</title>
+</head>
+<body>
+
+<!-- Creare un array di array. Ogni array figlio avrà come chiave una data in questo formato: DD-MM-YYYY es 01-01-2007 e come valore un array di post associati a quella data. Stampare ogni data con i relativi post. -->
+
+<?php
+
+$posts = [
+
+    '10/01/2019' => [
+        [
+            'title' => 'Post 1',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 1'
+        ],
+        [
+            'title' => 'Post 2',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 2'
+        ],
+    ],
+    '10/02/2019' => [
+        [
+            'title' => 'Post 3',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 3'
+        ]
+    ],
+    '15/05/2019' => [
+        [
+            'title' => 'Post 4',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 4'
+        ],
+        [
+            'title' => 'Post 5',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 5'
+        ],
+        [
+            'title' => 'Post 6',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 6'
+        ]
+    ],
+];
+
+// var_dump($posts['10/01/2019'][0]['title']);
+
+for($i = 0; $i < count($posts); $i++) {
+    // $data = $posts[$i];
+    $subarray = current($posts);
+    // var_dump($post); die;
+    // echo $posts[$i];
+    for($j = 0; $j < count($subarray); $j++){
+        echo $subarray[$j]["title"] . "<br>";
+        echo $subarray[$j]["author"] . "<br>";
+        echo $subarray[$j]["text"] . "<br>";
+    }
+}
+?>
+</body>
+</html>
